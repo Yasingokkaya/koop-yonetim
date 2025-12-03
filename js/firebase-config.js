b5af1f7861544b94dbd54d34dea68d4621b27790
Yasingokkaya <yasingokkaya91@gmail.com>
2025-12-03 20:49:29 +0300

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,6 +1,10 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth, onAuthStateChanged, signOut, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore, doc, getDoc, collection, addDoc, query, where, getDocs, onSnapshot, deleteDoc, updateDoc, arrayUnion, arrayRemove, orderBy, limit } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { 
+    getFirestore, doc, getDoc, collection, addDoc, query, where, getDocs, 
+    onSnapshot, deleteDoc, updateDoc, arrayUnion, arrayRemove, 
+    orderBy, limit, writeBatch 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAMTYZGCkVv1QausRJSyutrSUUvJ0mkqD4",
@@ -16,5 +20,11 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// orderBy ve limit eklendi
-export { auth, db, onAuthStateChanged, signOut, signInWithEmailAndPassword, doc, getDoc, collection, addDoc, query, where, getDocs, onSnapshot, deleteDoc, updateDoc, arrayUnion, arrayRemove, orderBy, limit };
+// writeBatch ve getDocs ARTIK BURADA DIŞARI AKTARILIYOR
+export { 
+    app, auth, db, 
+    onAuthStateChanged, signOut, signInWithEmailAndPassword,
+    doc, getDoc, collection, addDoc, query, where, getDocs, 
+    onSnapshot, deleteDoc, updateDoc, arrayUnion, arrayRemove, 
+    orderBy, limit, writeBatch 
+};
